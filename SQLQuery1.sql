c5aa98dba097c919912ea44a5287ab3f41a48efb
SRI VYSHNAV VEERAVALLI <97904310+vyshnavveeravalli@users.noreply.github.com>
2024-02-11 15:14:21 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+create database pizza_sales;
+select * from pizza_sales;
+select sum(total_price) as Total_revenue from pizza_sales;	
+select (sum(total_price) / count(distinct order_id)) as AVG_order_value from pizza_sales;
+select sum(quantity) as total_pizzas_sold from pizza_sales;
+select sum(distinct order_id) as total_orders from pizza_sales;
+select cast(cast(sum(quantity) as decimal(10,2))/ cast(count(distinct order_id) as decimal(10,2)) as decimal(10,2)) as avg_pizza_per_order from pizza_sales;
+select datename(dw, order_date) as order_day, count(distinct order_id) as total_orders from pizza_sales group by DATENAME(dw, order_date);
+select DATENAME(month, order_date) as month_name, count(distinct order_id) as total_orders from pizza_sales group by DATENAME(month, order_date);
+SELECT pizza_category, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue, CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT FROM pizza_sales GROUP BY pizza_category;
+select pizza_size, cast(sum(total_price) as decimal(10,2)) as total_revenue, cast(sum(total_price) * 100/(select sum(total_price) from pizza_sales) as decimal(10,2)) as PCT from pizza_sales group by pizza_size order by pizza_size;
+select pizza_category, sum(quantity) as total_quantity_sold from pizza_sales group by pizza_category order by total_quantity_sold desc;
+select pizza_name, sum(total_price) as total_revenue from pizza_sales group by pizza_name order by total_revenue desc limit 5;
+select  pizza_name, sum(total_price) as total_revenue from pizza_sales group by pizza_name order by total_revenue asc limit 5;
+select  pizza_name, sum(quantity) as total_pizza_sold from pizza_sales group by pizza_name order by total_pizza_sold desc limit 5;
+select  pizza_name, sum(quantity) as total_pizza_sold from pizza_sales group by pizza_name order by total_pizza_sold asc limit 5;
+select  pizza_name, count(distinct order_id) as total_order from pizza_sales group by pizza_name order by total_order desc limit 5;
+select  pizza_name, count(distinct order_id) as total_order from pizza_sales group by pizza_name order by total_order asc limit 5;
